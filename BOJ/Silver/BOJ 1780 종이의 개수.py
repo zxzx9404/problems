dc09883e8569f21e16e2df581e09009605e035a9
zxzx9404 <zxzx9404@gmail.com>
2022-08-16 17:51:16 +0900

    BOJ 1870 종이 자르기

---
+++ BOJ/Silver/BOJ 1780 종이의 개수.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+
+def papercheck(x, y, n):
+    global minus, zero, one
+    gijun = arr[x][y]
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if arr[i][j] != gijun:
+                for m in range(3):
+                    for l in range(3):
+                        papercheck(x+(m*n)//3, y+(l*n)//3, n//3)
+                return None
+
+    if gijun == -1:
+        minus += 1
+    elif gijun == 0:
+        zero += 1
+    elif gijun == 1:
+        one += 1
+
+n = int(input())
+
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+minus, zero, one = 0, 0, 0
+
+papercheck(0, 0, n)
+
+print(minus)
+print(zero)
+print(one)
