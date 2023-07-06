34650ad619e56eb82c6c4e1dc92e5837a6d34fb2
zxzx9404 <109266741+zxzx9404@users.noreply.github.com>
2023-07-06 12:18:18 +0900

    풀이 추가

--- BOJ/Silver/BOJ 1182 부분수열의 합.py
+++ BOJ/Silver/BOJ 1182 부분수열의 합.py
@@ -11,3 +11,30 @@ for i in range(1<<n):                  # 1<<n : 부분 집합의 개수
         cnt += 1
 
 print(cnt)
+
+'''
+- 23/07/06 추가 풀이
+
+def dfs(index, sum_of_num, flag):
+    global cnt
+    
+    if flag and sum_of_num == S:
+        cnt += 1
+        
+    if index == N:
+        return
+
+    dfs(index+1, sum_of_num, False)
+    dfs(index+1, sum_of_num + arr[index], True)
+    
+
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+cnt = 0
+dfs(0, 0, False)
+print(cnt)
+
+- 전체 부분집합을 구하지 않고, 재귀 호출을 통해 값만 확인하는 방식
+- 기존 코드 대비 시간 대폭 감소 3640ms -> 252ms
+
+'''
