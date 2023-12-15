e57b3907b5cc269fc12dfa140c4a4dfa4fc7d5b2
zxzx9404 <109266741+zxzx9404@users.noreply.github.com>
2023-12-15 13:52:14 +0900

    Create 2615. Sum of Distances.cpp

---
+++ Leetcode/2615. Sum of Distances.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long> ans(n);
+        unordered_map<int, vector<int>> d;
+        for (int i = 0; i < n; ++i) {
+            d[nums[i]].push_back(i);
+        }
+        for (auto& [_, idx] : d) {
+            int m = idx.size();
+            long long left = 0;
+            long long right = -1LL * m * idx[0];
+            for (int i : idx) {
+                right += i;
+            }
+            cout << "value : " << _ << endl; 
+            for (int i = 0; i < m; ++i) {
+                ans[idx[i]] = left + right;
+                cout << "left : " << left << " right : " << right << endl;
+                if (i + 1 < m) {
+                    left += (idx[i + 1] - idx[i]) * (i + 1);
+                    right -= (idx[i + 1] - idx[i]) * (m - i - 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
